7f39a978dc67bd5c72edbbecd68f852fd055b950
Kostadin_Veselinov <veselinovkostadin@gmail.com>
2024-02-15 15:26:06 +0100

    survey finish popup dialog

--- src/app/surveys/[surveyId]/questions/page.tsx
+++ src/app/surveys/[surveyId]/questions/page.tsx
@@ -5,7 +5,7 @@ import { FormEventHandler, useEffect,useState } from "react";
 
 export default function PublicSurveysPage() {
   const [questions, setQuestions] = useState([]);
-  const [arrIndex,setIndex] = useState<number>(0);
+  const [QIndex,setIndex] = useState<number>(0);
 //   const [answer,setAnswer] = useState<string>();
   const { surveyId } = useParams();
   
@@ -22,7 +22,7 @@ export default function PublicSurveysPage() {
         const formData = new FormData(currentTarget);
         const answer = formData.get("answer") as string;
 
-        const questionId = questions[arrIndex].id
+        const questionId = questions[QIndex].id
 
 
         const response = await fetch(`/api/surveys/${surveyId}/questions/${questionId}/answers`, {
@@ -40,24 +40,24 @@ export default function PublicSurveysPage() {
   },[]);
 
   useEffect(()=>{
-    if (arrIndex == questions.length - 1){
-        const submitBtn = document.getElementById("submitBtn")
-        submitBtn.innerText = "Finish"
+    if (questions.length > 0){
+        if (QIndex == questions.length){
+            const popup = document.getElementById("finish-popup")
+            popup.classList.remove("hidden")
+        }
+        const textArea = document.getElementById("areaAnswer")
+        textArea.value = ""
     }
-
-    // if(arrIndex >= questions.length){
-
-    // }
-
-  },[arrIndex])
+    
+  },[QIndex,questions])
 
 
   return (
     <div className="w-10/12 h-10/12 flex space-y-8 flex-col">
       <div className="bg-white flex flex-col items-center justify-center space-y-4 rounded-xl shadow pt-5">
-        <h3 className="text-md text-center text-emerald-600">Question {arrIndex}</h3>
+        <h3 className="text-md text-center text-emerald-600">Question {QIndex}</h3>
 
-        <h4 className="pt-10">{questions[arrIndex]?.text ?? "No questions found."} <span className="danger">{questions[arrIndex]?.required ? "*" : ""}</span></h4>
+        <h4 className="pt-10">{questions[QIndex]?.text ?? "No questions found."} <span className="danger">{questions[QIndex]?.required ? "*" : ""}</span></h4>
 
        
         <form className="flex flex-col gap-5 w-10/12" onSubmit={handleNextQuestion}>
@@ -66,11 +66,11 @@ export default function PublicSurveysPage() {
                 name="answer"
                 rows={4}
                 className="w-full rounded border-[1.5px] border-stroke bg-transparent py-3 px-5 font-medium outline-none disabled:cursor-defaut"
-                required={questions[arrIndex]?.required || false}
+                required={questions[QIndex]?.required || false}
             ></textarea>
             <div className="w-full flex justify-end items-center py-2 pb-4">
                 <button id="submitBtn" type="submit" className={"px-12 py-2 text-white rounded-md mt-5 shadow uppercase"} style={{backgroundColor:'rgb(4 120 87)'}}
-                disabled={arrIndex >= questions.length}>next</button>
+                disabled={QIndex >= questions.length}>{QIndex == questions.length - 1 ? "Finish" : "next"}</button>
             </div>
         </form>
         
--- src/app/surveys/layout.tsx
+++ src/app/surveys/layout.tsx
@@ -6,20 +6,19 @@ export default function PublicSurveyLayout({
 }: {
   children: React.ReactNode;
 }) {
+
   return (
-    <div className="w-screen h-screen" style={{backgroundImage: 'url("/images/background/green_bg.png")'}}>
+    <div className="w-screen h-screen relative" style={{backgroundImage: 'url("/images/background/green_bg.png")'}}>
       <div className="w-full h-full flex justify-center items-center z-0">
         <div className="w-4/5 h-4/5 flex justify-center items-center rounded-lg z-0" style={{backgroundColor: "rgba(229,229,229,0.6)",boxShadow:"0px 0px 12px rgba(0,0,0,0.7)"}}>
           {children}
         </div>
       </div>
 
-      <div className=" w-full h-full z-10" id="finish-popup" style={{backgroundColor:"rgba(0,0,0,0.5)"}}>
-        <div className="flex justify-center alignt-center">
-            <div className="bg-white rounded-md w-4/5 h-4/5 text-center">
-                <h3>Thank you for participating in this survey!</h3>
-            </div>
-        </div>
+      <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center z-10 d-none hidden" id="finish-popup" style={{backgroundColor:"rgba(0,0,0,0.5)"}}>
+          <div className="bg-white rounded-md w-max h-max p-8 text-center">
+              <h3>Thank you for participating in this survey!</h3>
+          </div>
       </div>
 
     </div>
